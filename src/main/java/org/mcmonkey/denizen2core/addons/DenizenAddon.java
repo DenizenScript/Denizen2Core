ea9fd983dce6b3fb9f5819a78caa765b1e71b4d8
Morphan1 <morphanone@gmail.com>
2016-08-27 19:52:15 -0400

    Add useful addon methods

--- src/main/java/org/mcmonkey/denizen2core/addons/DenizenAddon.java
+++ src/main/java/org/mcmonkey/denizen2core/addons/DenizenAddon.java
@@ -1,8 +1,11 @@
 package org.mcmonkey.denizen2core.addons;
 
 import org.mcmonkey.denizen2core.Denizen2Implementation;
+import org.mcmonkey.denizen2core.utilities.debugging.Debug;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public abstract class DenizenAddon {
 
@@ -19,22 +22,6 @@ public abstract class DenizenAddon {
         ((AddonClassLoader) classLoader).initialize(this);
     }
 
-    public void enable() {
-    }
-
-    public void reload() {
-    }
-
-    public void disable() {
-    }
-
-    final void init(AddonClassLoader classLoader, Denizen2Implementation impl, AddonInfo addonInfo, File dataFolder) {
-        this.addonClassLoader = classLoader;
-        this.implementation = impl;
-        this.addonInfo = addonInfo;
-        this.dataFolder = dataFolder;
-    }
-
     public AddonClassLoader getAddonClassLoader() {
         return addonClassLoader;
     }
@@ -49,5 +36,46 @@ public abstract class DenizenAddon {
 
     public File getDataFolder() {
         return dataFolder;
+    }
+
+    public void saveDefaultConfig() {
+        saveResource("config.yml", false);
+    }
+
+    public void saveResource(String resource, boolean overwrite) {
+        try (InputStream inputStream = addonClassLoader.getResourceAsStream(resource)) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Resource '" + resource + "' could not be found!");
+            }
+            File output = new File(getDataFolder(), resource);
+            if (output.exists()) {
+                if (!overwrite) {
+                    Debug.error("Resource '" + resource + "' already exists!");
+                    return;
+                }
+                output.delete();
+            }
+            Files.copy(inputStream, output.toPath());
+        }
+        catch (Exception e) {
+            Debug.error("Error while saving resource '" + resource + "'.");
+            Debug.exception(e);
+        }
+    }
+
+    public void enable() {
+    }
+
+    public void reload() {
+    }
+
+    public void disable() {
+    }
+
+    final void init(AddonClassLoader classLoader, Denizen2Implementation impl, AddonInfo addonInfo, File dataFolder) {
+        this.addonClassLoader = classLoader;
+        this.implementation = impl;
+        this.addonInfo = addonInfo;
+        this.dataFolder = dataFolder;
     }
 }
